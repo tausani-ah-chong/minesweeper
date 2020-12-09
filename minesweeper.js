fda728120cf0bf080345a2c0f4a5b08fc099cc26
Tausani Ah Chong <tausani93@gmail.com>
2020-12-10 09:39:40 +1300

    refactor cells

--- minesweeper.js
+++ minesweeper.js
@@ -3,15 +3,62 @@ document.addEventListener('DOMContentLoaded', startGame)
 // Define your `board` object here!
 var board = {
   cells: [
-    { row: 1, col: 1, isMine: false, hidden: true},
-    { row: 1, col: 2, isMine: false, hidden: true},
-    { row: 1, col: 3, isMine: false, hidden: true},
-    { row: 2, col: 1, isMine: false, hidden: true},
-    { row: 2, col: 2, isMine: false, hidden: true},
-    { row: 2, col: 3, isMine: false, hidden: true},
-    { row: 3, col: 1, isMine: false, hidden: true},
-    { row: 3, col: 2, isMine: false, hidden: true},
-    { row: 3, col: 3, isMine: false, hidden: true}
+    { 
+      row: 1,
+      col: 1,
+      isMine: false,
+      isMarked: false,
+      hidden: true
+    },
+    { 
+      row: 1,
+      col: 2,
+      isMine: false,
+      isMarked: false,
+      hidden: true
+    },
+    { 
+      row: 1,
+      col: 3,
+      isMine: false,
+      isMarked: false,
+      hidden: true},
+    { 
+      row: 2,
+      col: 1,
+      isMine: false,
+      isMarked: false,
+      hidden: true},
+    { 
+      row: 2,
+      col: 2,
+      isMine: false,
+      isMarked: false,
+      hidden: true},
+    { 
+      row: 2, 
+      col: 3,
+      isMine: false,
+      isMarked: false,
+      hidden: true},
+    { 
+      row: 3,
+      col: 1,
+      isMine: false,
+      isMarked: false,
+      hidden: true},
+    { 
+      row: 3,
+      col: 2,
+      isMine: false,
+      isMarked: false,
+      hidden: true},
+    { 
+      row: 3,
+      col: 3,
+      isMine: false,
+      isMarked: false,
+      hidden: true}
   ]
 } 
 
